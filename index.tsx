594e39d21dbe9cd169d095b8fc7345a9bb4839f6
Daichi Kudo <sakanactor624@gmail.com>
2020-08-29 17:54:17 +0900

    change parcel from webpack

---
+++ index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./src/App";
+
+// assets
+import "./src/assets/icon.png";
+import "./src/assets/works/blog.png";
+import "./src/assets/works/board.png";
+import "./src/assets/works/burger-builder.png";
+import "./src/assets/works/html-table-creator.png";
+import "./src/assets/works/slack-icon.png";
+
+ReactDOM.render(<App />, document.getElementById("app"));
