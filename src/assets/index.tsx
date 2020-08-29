a619f640e40487d6e2cc09b929aadd69448ff772
Daichi Kudo <sakanactor624@gmail.com>
2020-08-29 22:09:57 +0900

    bundle assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,7 @@
+import icon from "./icon.png";
+import blog from "./blog.png";
+import board from "./board.png";
+import burgerBuilder from "./burger-builder.png";
+import htmlTableCreator from "./html-table-creator.png";
+import SlackIcon from "./slack-icon.png";
+export { icon, blog, board, burgerBuilder, htmlTableCreator, SlackIcon };
