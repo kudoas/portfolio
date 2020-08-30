fdee0c108218fa30a1f66d3fb01c9713d4376870
Daichi Kudo <sakanactor624@gmail.com>
2020-08-30 23:20:52 +0900

    create timeline

--- src/sections/Experiences.tsx
+++ src/sections/Experiences.tsx
@@ -1,22 +1,75 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Timeline, TimelineEvent } from "react-event-timeline";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import CreateIcon from "@material-ui/icons/Create";
+import VpnKeyIcon from "@material-ui/icons/VpnKey";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
 
 import { StyledTitle } from "../components/Title";
-import { StyledTable } from "../components/Table";
 
 export const Expreience: React.FCX = ({ className }) => {
-  const tableLists = [
-    [["2020/08"], ["VOYAGE GROUP Treasure"]],
-    [["2020/09"], ["Rakuten 夏の陣 2020"]],
+  const myExperiences = [
+    {
+      title: "楽天インターン",
+      icon: <DirectionsRunIcon style={{ color: "blue" }} />,
+      period: "2020/09",
+      content: "新規プロトタイプ開発インターン",
+      link: "#",
+    },
+    {
+      title: "VOYAGE GROUP Treasure",
+      icon: <DirectionsRunIcon style={{ color: "blue" }} />,
+      period: "2020/08",
+      content: "事前講義を含む、プロダクト開発型インターン",
+      link: "#",
+    },
+    {
+      title: " Milliman, Inc. systematic review",
+      icon: <CreateIcon style={{ color: "black" }} />,
+      period: "2019/07 - 2019/12",
+      content: "糖尿病関連の論文を調査し、データをまとめるアルバイト",
+      link: "#",
+    },
+    {
+      title: "公衆衛生・疫学研究室 配属",
+      icon: <VpnKeyIcon style={{ color: "orange" }} />,
+      period: "2019/04",
+      content:
+        "医療系データ（レセプト、検診データ等）を使用し、薬剤の費用対効果の探索や治療実態の調査を行う",
+      link: "#",
+    },
+    {
+      title: "Darn My Engineering Life",
+      icon: <FavoriteIcon style={{ color: "red" }} />,
+      period: "2019/02",
+      content: "プログラミングの楽しさに気付き、学習を開始する",
+      link: "#",
+    },
   ];
 
   return (
     <section className={className + " " + "#experiences"}>
       <StyledTitle tag="h2">Experiences</StyledTitle>
-      <div className="intern">
-        <StyledTitle tag="h3">インターン</StyledTitle>
-        <StyledTable tableLists={tableLists} />
-      </div>
+      <Timeline>
+        {myExperiences.map((ex) => {
+          return (
+            <TimelineEvent
+              key={ex.title}
+              title={ex.title}
+              createdAt={ex.period}
+              icon={ex.icon}
+              iconColor="#09f"
+            >
+              <Typography key={ex.title} component="p" variant="inherit">
+                {ex.content}
+              </Typography>
+            </TimelineEvent>
+          );
+        })}
+      </Timeline>
     </section>
   );
 };
@@ -24,8 +77,5 @@ export const Expreience: React.FCX = ({ className }) => {
 export const StyledExpreience = styled(Expreience)`
   .intern {
     text-align: center;
-    table {
-      margin: 20px auto;
-    }
   }
 `;
