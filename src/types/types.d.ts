49b27b76cdcde2ef906d95c2bdfea8ce43b5d976
Daichi Kudo <sakanactor624@gmail.com>
2024-02-04 12:32:46 +0900

    fix ci

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,5 +3,5 @@
 import * as React from "react";
 
 declare module "react" {
-  type FCX<P = {}> = FunctionComponent<P & { className?: string, children?: ReactNode }>;
+  type FCX<P = {}> = FunctionComponent<P & { className?: string; children?: ReactNode }>;
 }
