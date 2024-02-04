ae642193f025a61ff33ca26b51f0271bac084b9d
Daichi Kudo <sakanactor624@gmail.com>
2024-02-04 12:50:59 +0900

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      external: ['@material-ui/styles']
-    }
   },
   plugins: [react()],
 });
