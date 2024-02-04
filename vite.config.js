e533f75a232d82752986e8a5bc179a5943ac6050
Daichi Kudo <sakanactor624@gmail.com>
2024-02-04 13:00:35 +0900

    node_modules/@material-ui/styles/index.js にリプレイスする

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     outDir: 'dist',
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      find: /^@material-ui\/styles(.*)$/,
+      replacement: resolve(__dirname, '.node_modules/@material-ui/styles/index.js'),
+    }
+  }
 });
