f2333149b9dc2f5f32dec86b3d853a395d8ad80e
Daichi Kudo <sakanactor624@gmail.com>
2024-02-09 21:02:55 +0900

    refactor: data と 型を切り出す

---
+++ src/data/skills.ts
@@ -0,0 +1,38 @@
+export const skills = [
+  {
+    name: "Python: Django, Flask, Pandas",
+    description: "Webアプリ, 競技プログラミング, 医療系データの解析",
+  },
+  {
+    name: "TypeScript: React.js, Node.js, Express.js",
+    description: "Webアプリ、API",
+  },
+  {
+    name: "JavaScript: React.js, Node.js, Express.js, Gatsby.js",
+    description: "Webアプリ、API",
+  },
+  {
+    name: "Go",
+    description: "API開発",
+  },
+  {
+    name: "HTML / CSS",
+    description: "フロントデザイン, Styled Component",
+  },
+  {
+    name: "MySQL, PostgresQL, MongoDB",
+    description: "Webサービスのデータベース",
+  },
+  {
+    name: "Heroku, AWS S3, Netlify, Firebase",
+    description: "インフラ",
+  },
+  {
+    name: "Google Apps Script",
+    description: "slack bot開発：積読管理、記事レコメンド)",
+  },
+  {
+    name: "Google Analytics",
+    description: "モニタリング",
+  },
+];
