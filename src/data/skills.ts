4ae537a613189e8e7f78337dd346340259a66049
Daichi Kudo <sakanactor624@gmail.com>
2024-02-23 12:43:35 +0900

    fix: skill content

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,38 +1,26 @@
 export const skills = [
   {
-    name: "Python: Django, Flask, Pandas",
-    description: "Webアプリ, 競技プログラミング, 医療系データの解析",
+    name: "Language",
+    description: "TypeScript, JavaScript, Ruby, Python, Go",
   },
   {
-    name: "TypeScript: React.js, Node.js, Express.js",
-    description: "Webアプリ、API",
+    name: "Framework",
+    description: "Rails, Angular, React",
   },
   {
-    name: "JavaScript: React.js, Node.js, Express.js, Gatsby.js",
-    description: "Webアプリ、API",
+    name: "Infrastructure",
+    description: "AWS, Docker, Vercel",
   },
   {
-    name: "Go",
-    description: "API開発",
+    name: "CI / CD",
+    description: "GitHub Actions, CircleCI",
   },
   {
-    name: "HTML / CSS",
-    description: "フロントデザイン, Styled Component",
+    name: "Database",
+    description: "MySQL, PostgreSQL",
   },
   {
-    name: "MySQL, PostgresQL, MongoDB",
-    description: "Webサービスのデータベース",
-  },
-  {
-    name: "Heroku, AWS S3, Netlify, Firebase",
-    description: "インフラ",
-  },
-  {
-    name: "Google Apps Script",
-    description: "slack bot開発：積読管理、記事レコメンド)",
-  },
-  {
-    name: "Google Analytics",
-    description: "モニタリング",
+    name: "Others",
+    description: "REST API, GraphQL",
   },
 ];
