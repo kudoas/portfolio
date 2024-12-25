f33048d00e49835ac94a2e4d8f7d645c59cc6710
Daichi Kudo <sakanactor624@gmail.com>
2024-12-25 22:42:42 +0900

    chore: setup tailwindcss & delete scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Da1chi';
