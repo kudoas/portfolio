56c8bae138a2317b223534a68cc332ea51625eb9
Daichi Kudo <sakanactor624@gmail.com>
2025-03-02 15:15:53 +0900

    chore: migrate project to Analog.js with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import { swcAngularUnpluginOptions } from '@jscutlery/swc-angular';
 import swc from 'unplugin-swc';
+import { swcAngularUnpluginOptions } from '@jscutlery/swc-angular';
+import analog from '@analogjs/platform';
 
+<<<<<<< HEAD
 export default defineConfig({
   plugins: [swc.vite(swcAngularUnpluginOptions())],
   test: {
@@ -14,19 +16,59 @@ export default defineConfig({
       provider: 'istanbul',
       include: ['src/**/*.ts'],
       exclude: ['src/main.ts'],
+=======
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    cacheDir: './node_modules/.vite',
+    build: {
+      outDir: './dist/./client',
+      reportCompressedSize: true,
+      target: ['es2020'],
+>>>>>>> 5d28753 (chore: migrate project to Analog.js with Vite)
     },
-    browser: {
-      instances: [
-        {
-          browser: 'chrome',
+    plugins: [
+      analog({
+        ssr: false,
+        static: true,
+        prerender: {
+          routes: [],
         },
-        {
-          browser: 'firefox',
-        },
-      ],
-      provider: 'webdriverio',
-      enabled: true,
-      headless: true,
+      }),
+      viteTsConfigPaths(),
+      swc.vite(swcAngularUnpluginOptions())
+    ],
+    server: {
+      fs: {
+        allow: ['.'],
+      },
     },
-  },
+    test: {
+      globals: true,
+      setupFiles: ['src/setup-vitest.ts'],
+      globalSetup: 'src/global-setup.ts',
+      include: ['src/**/*.spec.ts'],
+      coverage: {
+        provider: 'istanbul',
+        include: ['src/**/*.ts'],
+        exclude: ['src/main.ts'],
+      },
+      browser: {
+        instances: [
+          {
+            browser: 'chrome',
+          },
+        ],
+        provider: 'webdriverio',
+        enabled: true,
+        headless: true,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
 });
