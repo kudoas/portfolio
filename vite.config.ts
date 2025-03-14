2a117c019665720ba15b368cf63c23e717e8c002
Daichi Kudo <45157831+kudoas@users.noreply.github.com>
2025-03-14 17:40:05 +0900

    Migrate project to Analog.js with Vite and implement Zenn articles API (#514)
    * chore: migrate project to Analog.js with Vite
    
    * feat: add styles.css link to index.html
    
    * chore: remove Nx and jsdom dependencies
    
    * feat: implement Zenn articles API route and remove proxy configuration
    
    * chore: update output path in angular.json to 'dist'
    
    * chore: clean up vite.config.ts by removing unused test configuration
    
    * chore: remove unused globalSetup configuration from vite.config.ts
    
    * fix: change static option to false in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,63 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import { swcAngularUnpluginOptions } from '@jscutlery/swc-angular';
 import swc from 'unplugin-swc';
+import { swcAngularUnpluginOptions } from '@jscutlery/swc-angular';
+import analog from '@analogjs/platform';
 
-export default defineConfig({
-  plugins: [swc.vite(swcAngularUnpluginOptions())],
-  test: {
-    globals: true,
-    setupFiles: ['src/setup-vitest.ts'],
-    include: ['src/**/*.spec.ts'],
-    coverage: {
-      provider: 'istanbul',
-      include: ['src/**/*.ts'],
-      exclude: ['src/main.ts'],
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    cacheDir: './node_modules/.vite',
+    build: {
+      outDir: './dist/./client',
+      reportCompressedSize: true,
+      target: ['es2020'],
     },
-    browser: {
-      instances: [
-        {
-          browser: 'chrome',
+    plugins: [
+      analog({
+        ssr: false,
+        static: false,
+        prerender: {
+          routes: [],
         },
-        {
-          browser: 'firefox',
-        },
-      ],
-      provider: 'webdriverio',
-      enabled: true,
-      headless: true,
+        nitro: {
+          preset: 'vercel'
+        }
+      }),
+      viteTsConfigPaths(),
+      swc.vite(swcAngularUnpluginOptions())
+    ],
+    server: {
+      fs: {
+        allow: ['.'],
+      },
     },
-  },
+    test: {
+      globals: true,
+      setupFiles: ['src/setup-vitest.ts'],
+      include: ['src/**/*.spec.ts'],
+      coverage: {
+        provider: 'istanbul',
+        include: ['src/**/*.ts'],
+        exclude: ['src/main.ts'],
+      },
+      browser: {
+        instances: [
+          {
+            browser: 'chrome',
+          },
+        ],
+        provider: 'webdriverio',
+        enabled: true,
+        headless: true,
+      },
+    },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+    },
+  };
 });
